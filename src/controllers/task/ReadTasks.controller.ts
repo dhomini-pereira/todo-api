a6e59b130a905a8083104a164820f84230d47341
dhpe <dhpe@brasilcap.com.br>
2024-09-05 16:51:12 -0300

    fix: promise não funcional

--- src/controllers/task/ReadTasks.controller.ts
+++ src/controllers/task/ReadTasks.controller.ts
@@ -2,15 +2,15 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { TaskRepo } from "../../repos/Task.repo";
 import { database } from "../../config/database.config";
 
-
-
 export class ReadTasksController {
   public static async handle(request: FastifyRequest, reply: FastifyReply) {
     const { page } = request.query as { page: number };
     const userId = request.user.id;
 
-    const tasks = await new TaskRepo().read({ page, userId });
-    const count = await database.task.count({ where: { userId } });
+    const [tasks, count] = await Promise.all([
+      new TaskRepo().read({ page, userId }),
+      database.task.count({ where: { userId } }),
+    ]);
 
     return reply.send({
       total: count.valueOf(),
