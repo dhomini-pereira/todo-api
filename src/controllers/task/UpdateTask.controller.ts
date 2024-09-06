40ea5c4c2360f5549178d297282b6d0dfde55e02
dhpe <dhpe@brasilcap.com.br>
2024-09-06 10:53:11 -0300

    fix: solucionado problema de tipagem do params

--- src/controllers/task/UpdateTask.controller.ts
+++ src/controllers/task/UpdateTask.controller.ts
@@ -6,6 +6,7 @@ const taskSchema = z.object({
   id: z.number().int(),
   title: z.string().min(1).optional(),
   description: z.string().min(1).nullish(),
+  status: z.enum(["DONE", "PENDING", "CANCELED"]).optional(),
   userId: z.string().uuid(),
 });
 
@@ -15,11 +16,11 @@ export class UpdateTaskController {
   public static async handle(request: FastifyRequest, reply: FastifyReply) {
     const task: TaskSchema = taskSchema.parse({
       ...(request.body as any),
-      ...(request.params as any),
+      id: Number((request.params as any).id),
       userId: request.user.id,
     });
 
-    if (!task.title && !task.description)
+    if (!task.title && !task.description && !task.status)
       return reply.status(400).send({
         message: "There must be at least one field",
       });
