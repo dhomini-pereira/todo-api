172bdf252db3c53a83164ce24e730fb1f5680ab8
dhpe <dhpe@brasilcap.com.br>
2025-02-22 23:07:11 -0300

    fix: correção de infraestrutura

--- src/server.ts
+++ src/server.ts
@@ -86,6 +86,6 @@ io.on("connection", async (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log(`Listen on port: 3000`);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Listen on port: ${process.env.PORT || 3000}`);
 });
